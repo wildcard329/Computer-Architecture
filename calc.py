68554353d98dbc816cb4853cee6e94d37a082ffc
wildcard329 <enkiduwildman@gmail.com>
2020-10-16 10:45:36 -0700

    still working on sc

--- calc.py
+++ calc.py
@@ -8,20 +8,35 @@ remainder = [num]
 
 def detect_value(num, place_value):
     if num >= place_value:
-        num -= place_value
+        rem = num - place_value
         value.append(1)
-        remainder.append(num)
+        print(f"{place_value} can be subtracted from {num}, so subtract {place_value} from {num}, and {rem} is the remainder, carry the remainder.")
+        print(f"One is appended to our binary value, since we are solving from left to right and the value for the {place_value} bit is present.")
+        print('Value thus far, as represented by an array: \n',value)
+        input()
+        remainder.append(rem)
     else:
         value.append(0)
+        print(f"{place_value} cannot be subtracted from {num}.")
+        print(f"Zero is appended to binary value, since the {place_value} bit is not present.")
+        print('Value thus far, as represented by an array: ',value)
+        input()
 
 place_values = [2**x for x in range(8)]
 
+print(f"To convert an integer to a byte in binary, the method is simple.\nFirst start with an array from 0 to 7,\n{[x for x in range(8)]}\nthe values of these places represent the placeholders\nnumbered 0 through 7 for the powers of two, thus we will\nraise two to the power of these places\n{place_values}\n")
+print("\n(2 to the power of that position is the value for the bit in that position)\n\n")
+
 place_values = place_values[::-1]
+
+print(f'Reverse the array, the left-most bit will hold the highest value.\n{place_values}\nFor each item in this array, we compare the value of the\ninteger we wish to convert to binary, and subtract out the place value\nfrom that integer if the integer is greater than or equal to that value.\nThis will result in an array that has eight items, with each item having\na value of one or zero, similar to a binary number.\n')
 
 for x in place_values:
     detect_value(remainder[-1], x)
 
 if remainder[-1] == 0:
-    print(value)
+    print(value,'\n\nor\n')
+    value = [str(x) for x in value]
+    print('0b' + ''.join(value))
 else:
     print('Invalid input')
--- ls8/cpu.py
+++ ls8/cpu.py
@@ -2,38 +2,72 @@
 
 import sys
 
+SP = 0b00000111
+HLT = 0b00000001
+LDI = 0b10000010
+PRN = 0b01000111
+MUL = 0b10100010
+CMP = 0b10100111
+JEQ = 0b01010101
+JNE = 0b01010110
+JMP = 0b01010100
+POP = 0b01000110
+RET = 0b00010001
+PUSH = 0b01000101
+CALL = 0b01010000
+
 class CPU:
     """Main CPU class."""
 
     def __init__(self):
         """Construct a new CPU."""
         self.pc = 0
+        self.fl = 0
         self.ram = [None] * 256
         self.reg = [None] * 8
-        self.HLT = 1
-        self.LDI = 130
-        self.PRN = 71
+        self.reg[SP] = 0xF4
+        self.cmds = {}
+        self.cmds[LDI] = self.LDI_val
+        self.cmds[PRN] = self.PRN_val
+        self.cmds[MUL] = self.MUL_val
+        self.cmds[HLT] = self.HLT_val
+        self.cmds[POP] = self.POP_val
+        self.cmds[JEQ] = self.JEQ_val
+        self.cmds[JNE] = self.JNE_val
+        self.cmds[CMP] = self.CMP_val
+        self.cmds[PUSH] = self.PUSH_val
 
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        if len(sys.argv) != 2:
+            print("usage: ls8.py filename")
+            sys.exit(1)
 
-        # For now, we've just hardcoded a program:
+        try:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+            address = 0
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    t = line.split('#')
+                    n = t[0].strip()
+
+                    if n == '':
+                        continue
+
+                    try:
+                        n = int(n, 2)
+                    except ValueError:
+                        print(f"Invalid number: '{n}")
+                        sys.exit(1)
+
+                    self.ram_write(address, n)
+                    address += 1
+
+        except FileNotFoundError:
+            print(f'File not found: {sys.argv[1]}')
+            sys.exit(2)
 
     def ram_read(self, MAR):
         return self.ram[MAR]
@@ -47,6 +81,22 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
+        elif op == "SUB":
+            self.reg[reg_a] -= self.reg[reg_b]
+        elif op == "DIV":
+            self.reg[reg_a] //= self.reg[reg_b]
+        elif op == 'CMP':
+            if reg_a == reg_b:
+                self.fl | 0b001
+                self.fl & 0b001
+            elif reg_a < reg_b:
+                self.fl | 0b100
+                self.fl & 0b100
+            elif reg_a > reg_b:
+                self.fl | 0b010
+                self.fl & 0b010
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -70,20 +120,65 @@ class CPU:
 
         print()
 
+    def PRN_val(self, idx, arg2):
+        print(self.reg[idx])
+
+    def LDI_val(self, arg1, arg2):
+        self.reg[arg1] = self.ram_read(arg2)
+
+    def MUL_val(self, num1, num2):
+        self.alu("MUL", num1, self.ram_read(num2))
+
+    def HLT_val(self, arg1, arg2):
+        sys.exit(0)
+
+    def PUSH_val(self, arg1, arg2):
+        self.reg[SP] -= 1
+        val = self.reg[arg1]
+        self.ram[self.reg[SP]] = val
+
+    def POP_val(self, arg1, arg2):
+        val = self.ram[self.reg[SP]]
+        self.reg[arg1] = val
+        self.reg[SP] += 1
+
+    def CALL_val(self, arg1, arg2):
+        self.PUSH_val(arg2)
+        self.pc = arg1
+
+    def RET_val(self, arg1, arg2):
+        self.pc = self.POP_val()
+
+    def JMP_val(self, arg1, arg2):
+        self.pc = self.reg[arg1]
+
+    def JNE_val(self, arg1, arg2):
+        if self.fl == 0:
+            print('jne')
+            self.JMP_val(arg1, arg2)
+
+    def JEQ_val(self, arg1, arg2):
+        if self.fl == 1:
+            print('jeq')
+            self.JMP_val(arg1, arg2)
+
+    def CMP_val(self, arg1, arg2):
+        reg_a = self.reg[arg1]
+        reg_b = self.reg[self.ram_read(arg2)]
+        self.alu("CMP", reg_a, reg_b)
+
     def run(self):
         """Run the CPU."""
-        running = True
-        while running:
-            ir = self.ram_read(self.pc)
-            if ir == self.LDI:
-                reg_idx = self.ram_read(self.pc + 1)
-                reg_val = self.ram_read(self.pc + 2)
-                self.reg[reg_idx] = reg_val
-                self.pc += 3
-            elif ir == self.PRN:
-                print(self.reg[self.ram_read(self.pc + 1)])
-                self.pc += 2
-            elif ir == self.HLT:
-                running = False
-        # print(self.ram)
-        # print(self.reg)
+
+        while True:
+            ir = self.ram_read((self.pc))
+            argA = self.ram_read(self.pc + 1)
+            argB = self.pc + 2
+
+            if ir in self.cmds:
+                self.cmds[ir](argA, argB)
+                # print(self.reg[0])
+                # input()
+            if ir is not None:
+                i_len = ((ir & 0b11000000) >> 6) + 1
+                self.pc += i_len
